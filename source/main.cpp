030b7318323af869eb690b75f718731f0bcba923
Oliver Benns <oliverbenns@gmail.com>
2017-08-02 10:24:43 +0100

    Link glfw, opengl and output msg

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main() {
   printf("Hello world\n");
--- tools/mac/build.sh
+++ tools/mac/build.sh
@@ -1,9 +1,17 @@
 # !/bin/bash
+
 ROOT_DIR="$(dirname "$0")/../.."
 ENTRY_FILE="${ROOT_DIR}/source/main.cpp"
 OUTPUT_FILE="${ROOT_DIR}/build/main"
+ERROR_TEXT="\033[0;31m"
+SUCCESS_TEXT="\033[0;32m"
+DEFAULT_TEXT="\033[0m"
 
-gcc "${ENTRY_FILE}" -o "${OUTPUT_FILE}"
+gcc "${ENTRY_FILE}" -o "${ROOT_DIR}/build/main" -lglfw -framework OpenGL
 
-DATE=`date +%H:%M:%S`
-echo "Built on ${DATE}"
+if [ $? -eq 0 ]; then
+  DATE=`date +%H:%M:%S`
+  echo "${SUCCESS_TEXT}Built on ${DATE}${DEFAULT_TEXT}"
+else
+  echo "${ERROR_TEXT}Compilation failed${DEFAULT_TEXT}"
+fi
