d5fcd954aaf577a2fa40dd1f50f2f4d0931fcd35
Oliver Benns <oliverbenns@gmail.com>
2017-08-02 10:24:08 +0100

    Add Xcode cli tools to install

--- tools/mac/install.sh
+++ tools/mac/install.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 
 # https://stackoverflow.com/questions/23450334/opengl-3-3-4-1-on-mac-osx-10-9-using-glfw-library
+xcode-select --install
 brew update
 brew install --without-shared-library glfw3
