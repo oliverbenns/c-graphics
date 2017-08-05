5e6c450f90e4552eec733229f07b528d5dc1a54e
Oliver Benns <oliverbenns@gmail.com>
2017-08-05 16:16:09 +0100

    Replace epileptic window with pulse

--- source/main.cpp
+++ source/main.cpp
@@ -2,13 +2,25 @@
 #include <GLFW/glfw3.h>
 #include "display.h"
 
-int on = 0;
+// int on = 0;
+float value = 0;
+enum direction {FORWARD, REVERSE};
+enum direction foo = FORWARD;
 
 void render() {
-  int value = on > 0 ? 255 : 0;
   glClearColor(value, value, value, 1);
   glClear(GL_COLOR_BUFFER_BIT);
-  on = on > 0 ? 0: 1;
+  
+  value+= foo == FORWARD ? 0.015 : -0.015;
+
+  if (value < 0) {
+    foo = FORWARD;
+  }
+
+  if (value > 1) {
+    foo = REVERSE;
+  }
+
 }
 
 int main() {
