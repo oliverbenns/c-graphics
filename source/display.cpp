cf81f638f81f02236cf21cbf0384431e638c7027
Oliver Benns <oliverbenns@gmail.com>
2017-08-05 16:15:50 +0100

    Set GLFW OpenGL version

--- source/display.cpp
+++ source/display.cpp
@@ -11,6 +11,15 @@ int initDisplay(Display *display) {
     return 1;
   }
 
+  // Tell GLFW what version of OpenGL we are running.
+  // @TODO: We can probably get this from openGL somehow.
+  // @TODO: What openGL am I actually running? Homebrew is @3.2.1 but XCode is importing from Apple default (4.1)
+  // printf("OPEN GL VERSION: %s\n", glGetString(GL_VERSION));
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // This fixes OSX, see comments: https://learnopengl.com/#!Getting-started/Hello-Window
+
   display->window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, TITLE, 0, 0);
 
   if (!display->window) {
