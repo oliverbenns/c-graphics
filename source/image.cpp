8ad9ca12dbc8e4c569fbcd737a3009b2b45da454
Oliver Benns <oliverbenns@gmail.com>
2017-08-08 18:18:56 +0100

    Add image files

---
+++ source/image.cpp
@@ -0,0 +1,6 @@
+// @TODO
+// Read the file contents into memory
+// Decode the file to raw format required (RGB or RGBA)
+// Create OpenGL texture object and give the raw image data to OpenGL
+// Construct OpenGL primitive with appropriate texture coordinates and vertices positions.
+// Bind the texture to appropriate texture unit and use a sampler object to use the texture data to produce final color.
