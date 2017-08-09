e0e89e49c705f8a6daca0f7b67466fce6c64322a
Oliver Benns <oliverbenns@gmail.com>
2017-08-09 18:51:59 +0100

    Include stb image header

--- tools/mac/install.sh
+++ tools/mac/install.sh
@@ -1,6 +1,12 @@
 # !/bin/bash
 
+ROOT_DIR="$(dirname "$0")/../.."
+INCLUDE_DIR="${ROOT_DIR}/include"
+
 # https://stackoverflow.com/questions/23450334/opengl-3-3-4-1-on-mac-osx-10-9-using-glfw-library
 xcode-select --install
 brew update
 brew install --without-shared-library glfw3
+
+mkdir "${INCLUDE_DIR}"
+curl https://raw.githubusercontent.com/nothings/stb/master/stb_image.h -o "${INCLUDE_DIR}/stb_image.h"
